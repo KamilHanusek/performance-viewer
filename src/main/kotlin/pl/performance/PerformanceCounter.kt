0d409a9551317bd5565e1a7b376456e242979d41
kamil.hanusek@jakdojade.pl <Asd123zxc>
2020-10-05 18:10:32 +0200

    Fix count method

--- src/main/kotlin/pl/performance/PerformanceCounter.kt
+++ src/main/kotlin/pl/performance/PerformanceCounter.kt
@@ -2,23 +2,16 @@ package pl.performance
 
 import kotlin.system.measureTimeMillis
 
-internal class PerformanceCounter(
-    private val TEST_COUNTS: Set<Int> = DEFAULT_TEST_SET
-) {
-    companion object {
-        internal val DEFAULT_TEST_SET =
-            setOf(1000, 2000, 3000, 4000, 5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100_000)
-        internal val DEFAULT_TEST_SET_SIZE = DEFAULT_TEST_SET.size
-    }
+internal class PerformanceCounter {
 
     internal fun count(vararg jobToPerform: PerformJob): List<JobForCountWithTime> {
         return jobToPerform.map { job ->
             System.gc()
-            val timesByCount = TEST_COUNTS.map { count ->
-                val spentTime = measureTimeMillis { job.runXTimes(count) }
+            val timesByCount = job.tasks.map { task ->
+                val spentTime = measureTimeMillis { task.performance() }
                 JobForCountWithTime.CountTime(
-                    count = count,
-                    timeInMillis = spentTime
+                        count = task.xAxisValues,
+                        timeInMillis = spentTime
                 )
             }
             JobForCountWithTime(job.id, timesByCount)
@@ -26,18 +19,15 @@ internal class PerformanceCounter(
     }
 
     internal data class JobForCountWithTime(
-        val jobName: String,
-        val testCountsWithTimeTime: List<CountTime>,
+            val jobName: String,
+            val testCountsWithTimeTime: List<CountTime>,
     ) {
 
         internal data class CountTime(
-            val timeInMillis: Long,
-            val count: Int
+                val timeInMillis: Long,
+                val count: Number
         )
     }
 }
 
-private fun PerformJob.runXTimes(count: Int) {
-    (1..count).forEach { this.performance() }
-}
 
