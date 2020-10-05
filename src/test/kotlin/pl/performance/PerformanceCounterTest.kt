f24396e9a041b6589ade29ae35a0f4c9ac2d93f8
kamil.hanusek@jakdojade.pl <Asd123zxc>
2020-10-05 16:51:16 +0200

    Fix test

--- src/test/kotlin/pl/performance/PerformanceCounterTest.kt
+++ src/test/kotlin/pl/performance/PerformanceCounterTest.kt
@@ -16,25 +16,26 @@ internal class PerformanceCounterTest {
         val data = DataCreator.create(100) {
             Random.nextInt()
         }
-        val jobToPerform = PerformJob("something 1") {
-            val randomInt = Random.nextInt()
-            data.firstOrNull { randomInt == it }
-        }
+        val jobToPerform = PerformJob("something 1", tasks = listOf(
+                PerformJob.Task(xAxisValues = 3) {
+                    val randomInt = Random.nextInt()
+                    data.firstOrNull { randomInt == it }
+                }
+        ))
         val hashMap = hashMapOf(*data.map { it to it }.toTypedArray())
-        val jobToPerform2 = PerformJob("something 2") {
-            val randomInt = Random.nextInt()
-            hashMap.get(randomInt)
-        }
+        val jobToPerform2 = PerformJob("something 2", tasks = listOf(
+                PerformJob.Task(4){
+                    val randomInt = Random.nextInt()
+                    hashMap.get(randomInt)
+                }
+        ))
 
         // when
         val performanceResult = PerformanceCounter().count(jobToPerform, jobToPerform2)
-        val chart = XYChart(performanceResult.size, PerformanceCounter.DEFAULT_TEST_SET_SIZE)
-        performanceResult.forEach { (jobName, something) -> chart.addSeries(jobName, something.map { it.count }, something.map { it.timeInMillis }) }
-        SwingWrapper(chart).displayChart()
 
         // then
         assertThat(performanceResult.size).isEqualTo(2)
         assertThat(performanceResult.first().testCountsWithTimeTime).isNotEmpty()
-        assertThat(performanceResult.first().testCountsWithTimeTime.size).isEqualTo(PerformanceCounter.DEFAULT_TEST_SET_SIZE)
+        assertThat(performanceResult.get(1).testCountsWithTimeTime).isNotEmpty()
     }
 }
