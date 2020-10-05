cd6b4fb2469c66be2d52c60f184f87d8859cac9e
kamil.hanusek@jakdojade.pl <Asd123zxc>
2020-10-05 18:10:22 +0200

    Set theme and legend allignment

--- src/main/kotlin/pl/performance/PerformanceViewer.kt
+++ src/main/kotlin/pl/performance/PerformanceViewer.kt
@@ -3,16 +3,19 @@ package pl.performance
 import org.knowm.xchart.SwingWrapper
 import org.knowm.xchart.XYChart
 import org.knowm.xchart.XYChartBuilder
+import org.knowm.xchart.style.Styler
 
-class PerformanceViewer {
+object PerformanceViewer {
     private val performanceCounter: PerformanceCounter = PerformanceCounter()
 
-    fun performAndDisplay(vararg jobToPerform: PerformJob) {
+    fun performAndDisplay(xAxisName: String, vararg jobToPerform: PerformJob) {
         val performanceResult = performanceCounter.count(*jobToPerform)
         val chart = XYChart(XYChartBuilder().also {
-            it.xAxisTitle("UNKNOWN")
+            it.xAxisTitle(xAxisName)
             it.yAxisTitle("Time in miliseconds")
+            it.theme(Styler.ChartTheme.GGPlot2)
         })
+        chart.styler.legendPosition = Styler.LegendPosition.InsideN
         performanceResult.forEach { resultJob ->
             val counts = resultJob.testCountsWithTimeTime.map { it.count }
             val timesInMillis = resultJob.testCountsWithTimeTime.map { it.timeInMillis }
