e2c78800fba481e24bf45629667ba446207741ab
kamil.hanusek@jakdojade.pl <Asd123zxc>
2020-10-05 18:24:33 +0200

    Add possibility to show many graphs at one time

--- src/main/kotlin/pl/performance/PerformanceViewer.kt
+++ src/main/kotlin/pl/performance/PerformanceViewer.kt
@@ -10,12 +10,8 @@ object PerformanceViewer {
 
     fun performAndDisplay(xAxisName: String, vararg jobToPerform: PerformJob) {
         val performanceResult = performanceCounter.count(*jobToPerform)
-        val chart = XYChart(XYChartBuilder().also {
-            it.xAxisTitle(xAxisName)
-            it.yAxisTitle("Time in miliseconds")
-            it.theme(Styler.ChartTheme.GGPlot2)
-        })
-        chart.styler.legendPosition = Styler.LegendPosition.InsideN
+        val chart = createDefaultChart(xAxisName)
+
         performanceResult.forEach { resultJob ->
             val counts = resultJob.testCountsWithTimeTime.map { it.count }
             val timesInMillis = resultJob.testCountsWithTimeTime.map { it.timeInMillis }
@@ -23,5 +19,31 @@ object PerformanceViewer {
         }
 
         SwingWrapper(chart).displayChart()
+    }
+
+    private fun createDefaultChart(xAxisName: String): XYChart {
+        return XYChart(XYChartBuilder().also {
+            it.xAxisTitle(xAxisName)
+            it.yAxisTitle("Time in miliseconds")
+            it.theme(Styler.ChartTheme.GGPlot2)
+        }).also {
+            it.styler.legendPosition = Styler.LegendPosition.InsideN
+        }
+    }
+
+    fun performAndDisplayOnSeparateChart(vararg separatedJob: SeparatedJob) {
+        val chartsToShow = separatedJob.map { (xAxisName, job) ->
+            val performanceResult = performanceCounter.count(job)
+            val chart = createDefaultChart(xAxisName)
+
+            performanceResult.forEach { resultJob ->
+                val counts = resultJob.testCountsWithTimeTime.map { it.count }
+                val timesInMillis = resultJob.testCountsWithTimeTime.map { it.timeInMillis }
+                chart.addSeries(resultJob.jobName, counts, timesInMillis)
+            }
+            chart
+        }
+
+        SwingWrapper(chartsToShow).displayChartMatrix()
     }
 }
